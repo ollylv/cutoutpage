65b33221e9a4cf0a99bb0b7fc9b1fd387c68c22f
ollylv <olafs.grundans@gmail.com>
2016-12-15 09:33:09 +0200

    few functions here and there
    0.4

--- js/test.js
+++ js/test.js
@@ -8,24 +8,47 @@ $(document).ready(function(){
     var $sendButton = $('.send-button');
     var $commentCounterspan = $('.comment span');
     var $commentContainer = $('.comment-container');
-    var $alertFirst = $('.alert-first');
-    var $alertSecond = $('.alert-second');
+    var $alert = $('.alerts');
+    var $fill = $('.fill');
+    var $nospace = $('.nospace');
+    var $paragraph = $('.paragraph');
+    var $paragraphText = $('.paragraph p');
+    var $readLess = $('.readless');
+    var wordplacement = 57;
+    var textlength;
+    var readMore = 'Read&nbspMore';
 
+
+    putReadmore($paragraphText,wordplacement,readMore);
 
     //hidden on load
 
     $commentTextareawrapper.hide();
     $sendButton.hide();
-    $alertFirst.hide();
-    $alertSecond.hide();
+    $alert.hide();
+    $readLess.hide();
 
     //click listeners
 
-    $('.readmore').on('click', function(){
-        $(this).css('display',"none");
-        $('.paragraph').css('height',"auto");
-        $('.paragraph p span').css('display',"initial");
+    $paragraphText.find('.readmore').on('click', function(){
+        var $this = $(this);
+        $this.hide('fast', function(){
+            //css ;/ .hide() .show() turn display into inline-block, solution is to add a function, but it's still not the optimal way..
+            $paragraph.find('p span').css('display', 'inline').show(function(){
+                $readLess.show();
+            });
+        });
     });
+
+    $readLess.on('click', function(){
+        var $this = $(this);
+        $this.hide('fast', function(){
+            $paragraph.find('p span').hide('fast', function(){
+                $paragraphText.find('.readmore').show();
+            });
+        });
+    });
+
 
     $commentButton.on('click', function(){
         clearAll();
@@ -35,47 +58,58 @@ $(document).ready(function(){
     });
 
     $sendButton.on('click', function(){
-
+        clearAll();
         var notatrimmedValue = $commentTextarea.val();
         var trimmedValue = $commentTextarea.val().trim();
 
         if(notatrimmedValue){
-
             if(notatrimmedValue === trimmedValue){
-
                 // comment counter
                 var currCommentvalue = $commentCounterspan.text();
                 currCommentvalue = ++currCommentvalue;
                 $commentCounterspan.text(currCommentvalue);
-
                 //adding a comment
                 $commentContainer.append('<div class="comment-holder"><p>' + $commentTextarea.val() + '</p>' + '<span class="delete-button">' + 'X' + '</span></div>');
                 $commentTextareawrapper.toggle('fast', function(){
                     $sendButton.fadeToggle('fast');
                 });
 
+                clearAll();
             } else {
-                $alertSecond.fadeIn('fast');
+                $nospace.fadeIn('fast');
             }
         } else {
-            $alertFirst.fadeIn('fast');
+            $fill.fadeIn('fast');
         }
-
     });
 
-    // clear function
+    //focus
 
-    $commentTextarea.on('click', function(){
-        clearAll();
+    $commentTextarea.on('focus', function(){
+      clearAll();
     });
+
+    //functions
 
     function clearAll(){
-        $commentTextarea.val('');
-        $alertFirst.hide();
-        $alertSecond.hide();
-        $
+        $alert.hide();
     }
 
+
+    function putReadmore(textcontainer,wordplacement,readMore){
+
+        var wordplace = wordplacement;
+        var words = $.trim(textcontainer.html()).split(" ");
+        textlength = words.length;
+
+        if(textlength < wordplacement){wordplace = textlength}
+
+        words.splice(wordplace,0,'<a class="readmore">'+ readMore + '</a>');
+        words.splice(++wordplace,0,'<span>');
+        var wordsTostring = words.toString();
+        var text = (wordsTostring.replace(/,,/g,'&#44')).replace(/,/g,' ');
+        textcontainer.html(text);
+    }
 
 
     //comment deletion
